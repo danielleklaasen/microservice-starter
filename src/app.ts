4810e8b56565ef352e26aaa931207ec617de9e3a
danielleklaasen <danielle@ilumy.com>
2018-05-30 14:32:40 +0200

    mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import express = require('express');
-const app = express(); // Create Express server
-const cors = require('cors')
+import { NextFunction, Request, Response } from 'express';
+import mongoose = require('mongoose');
+import bluebird = require('bluebird');
 
-app.use(cors())
+const app = express(); // Create Express server
+
+// MongoDB connection
+(mongoose as any).Promise = bluebird;
+const mongoUrl = 'mongodb://admin:T5He1tMkFj9Ti9Ng@cluster0-shard-00-00-2mxug.mongodb.net:27017,cluster0-shard-00-01-2mxug.mongodb.net:27017,cluster0-shard-00-02-2mxug.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+mongoose.connect(mongoUrl).then(
+  () => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  },
+).catch(() => {
+  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+  // process.exit();
+});
 
 // Primary app routes
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send("hello world");
 });
+
+
 
 export default app;
