0fba82d05af8ae3ca01b24470982d3141f2b16f8
danielleklaasen <danielle@ilumy.com>
2018-05-31 18:15:23 +0200

    express setup adjustments

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,30 @@
+import { Handler } from 'aws-lambda';
 import * as bluebird from 'bluebird';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
-//const mongoose = require('mongoose');
-
+import * as serverless from 'serverless-http';
 import { default as Kitten } from './models/Kitten';
 
 const app = express(); // Create Express server
 
-// MongoDB setup
-bluebird.promisifyAll(mongoose as any); // use bluebird for promises
-const mongoUrl = 'mongodb://admin:T5He1tMkFj9Ti9Ng@cluster0-shard-00-00-2mxug.mongodb.net:27017,cluster0-shard-00-01-2mxug.mongodb.net:27017,cluster0-shard-00-02-2mxug.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-// MongoDB connection
-mongoose.connect(mongoUrl).then(
-  () => {
-    // Ready to use. The `mongoose.connect()` promise resolves to undefined.
-  },
-).catch((error) => {
-  // MongoDB connection error. Please make sure MongoDB is running.
-  process.exit();
-});
-
 // Express configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// MongoDB setup
+bluebird.promisifyAll(mongoose as any); // use bluebird for promises
+const mongoUrl = 'mongodb://admin:T5He1tMkFj9Ti9Ng@cluster0-shard-00-00-2mxug.mongodb.net:27017,cluster0-shard-00-01-2mxug.mongodb.net:27017,cluster0-shard-00-02-2mxug.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+
+// MongoDB connection
+mongoose.connect(mongoUrl).then(() => {
+  // Ready to use. The `mongoose.connect()` promise resolves to undefined.
+})
+.catch((error) => {
+  // MongoDB connection error. Please make sure MongoDB is running.
+  process.exit();
+});
 
 // Primary app routes
 app.get('/', (req: express.request, res: express.response) => {
@@ -35,9 +36,9 @@ app.post('/kitten', (req: express.request, res: express.response) => { // create
 
   kitty.saveAsync()
     .then((kitten) => {
-    res.send(kitten);
+      res.send(kitten);
     })
-    .catch((error)=>{
+    .catch((error) => {
       res.send(error);
     });
 });
@@ -47,19 +48,19 @@ app.get('/kittens', (req: express.request, res: express.response) => { // read
     .then((kittens) => {
       res.send(kittens);
     })
-    .catch((error)=>{
+    .catch((error) => {
       res.send(error);
     });
 });
 
 app.post('/kitten/:id', (req: express.request, res: express.response) => { // update
-  Kitten.findByIdAndUpdate(req.params.id, { name: req.body.name }, {new: true})
-    .then((kitten)=>{
+  Kitten.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true })
+    .then((kitten) => {
       res.send(kitten);
     })
-    .catch((error)=>{
-    res.send(error);
-  });
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 app.delete('/kitten/:id', (req: express.request, res: express.response) => { // delete
@@ -67,9 +68,9 @@ app.delete('/kitten/:id', (req: express.request, res: express.response) => { // 
     .then(() => {
       res.send('success');
     })
-    .catch((error)=>{
+    .catch((error) => {
       res.send(error);
     });
 });
 
-export default app;
+export const init: Handler = serverless(app);
