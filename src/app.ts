fddc3336c9acf63883a0bfd4ec815b481415b602
danielleklaasen <danielle@ilumy.com>
2018-05-31 14:53:11 +0200

    adding promises to crud system

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,21 @@ import * as bluebird from 'bluebird';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+//const mongoose = require('mongoose');
+
 import { default as Kitten } from './models/Kitten';
 
 const app = express(); // Create Express server
 
-// MongoDB connection
-(mongoose as any).Promise = bluebird;
+// MongoDB setup
+bluebird.promisifyAll(mongoose as any); // use bluebird for promises
 const mongoUrl = 'mongodb://admin:T5He1tMkFj9Ti9Ng@cluster0-shard-00-00-2mxug.mongodb.net:27017,cluster0-shard-00-01-2mxug.mongodb.net:27017,cluster0-shard-00-02-2mxug.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+// MongoDB connection
 mongoose.connect(mongoUrl).then(
   () => {
     // Ready to use. The `mongoose.connect()` promise resolves to undefined.
   },
-).catch((err) => {
+).catch((error) => {
   // MongoDB connection error. Please make sure MongoDB is running.
   process.exit();
 });
@@ -30,34 +33,43 @@ app.get('/', (req: express.request, res: express.response) => {
 app.post('/kitten', (req: express.request, res: express.response) => { // create
   const kitty = new Kitten({ name: req.body.name });
 
-  kitty.save((err, kitten) => {
-    if (err) return res.send(err);
+  kitty.saveAsync()
+    .then((kitten) => {
     res.send(kitten);
-  });
+    })
+    .catch((error)=>{
+      res.send(error);
+    });
 });
 
-app.get('/kitten/:id', (req: express.request, res: express.response) => { // read
-  Kitten.findById(req.params.id, (err, kitten) => {
-    res.send(kitten);
-  });
-});
-
-app.get('/kittens', (req: express.request, res: express.response) => { // read all
-  Kitten.find((err, kittens) => {
-    res.send(kittens);
-  });
+app.get('/kittens', (req: express.request, res: express.response) => { // read
+  Kitten.findAsync()
+    .then((kittens) => {
+      res.send(kittens);
+    })
+    .catch((error)=>{
+      res.send(error);
+    });
 });
 
 app.post('/kitten/:id', (req: express.request, res: express.response) => { // update
-  Kitten.findByIdAndUpdate(req.params.id, { name: req.body.name }, (err, kitten) => {
-    res.send(kitten);
+  Kitten.findByIdAndUpdate(req.params.id, { name: req.body.name }, {new: true})
+    .then((kitten)=>{
+      res.send(kitten);
+    })
+    .catch((error)=>{
+    res.send(error);
   });
 });
 
 app.delete('/kitten/:id', (req: express.request, res: express.response) => { // delete
-  Kitten.findByIdAndRemove(req.params.id, (err) => {
-    if (!err) res.send('success');
-  });
+  Kitten.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.send('success');
+    })
+    .catch((error)=>{
+      res.send(error);
+    });
 });
 
 export default app;
